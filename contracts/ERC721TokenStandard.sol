645ed67ee7b5835a93f1e16c8a102ebf3af81cb4
shogo <@example.com>
2019-02-13 23:05:31 +0900

    Change version of solidity.

--- contracts/ERC721TokenStandard.sol
+++ contracts/ERC721TokenStandard.sol
@@ -1,13 +1,26 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.4.25;
 
-import "openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721Token.sol";
 
 /**
  * @dev See https://github.com/ethereum/EIPs/blob/master/EIPS/eip-721.md
  */
-contract ERC721TokenStandard is ERC721Full {
-  constructor(string memory name, string memory symbol, uint tokenId, string memory tokenURI) ERC721Full(name, symbol) public {
-    _mint(msg.sender, tokenId);
-    _setTokenURI(tokenId, tokenURI);
+contract ERC721TokenStandard is ERC721Token {
+  uint256 internal nextTokenId = 0;
+
+  constructor() public ERC721Token("MyToken", "MTKN") {}
+
+  function mint() external {
+    uint256 tokenId = nextTokenId;
+    nextTokenId = nextTokenId.add(1);
+    super._mint(msg.sender, tokenId);
+  }
+
+  function setTokenURI(uint256 _tokenId, string _message) external {
+    super._setTokenURI(_tokenId, _message);
+  }
+
+  function burn(uint256 _tokenId) external {
+    super._burn(msg.sender, _tokenId);
   }
 }
--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.4.25;
 
 /**
  * @dev See https://github.com/truffle-box/react-box/blob/master/contracts/SimpleStorage.sol

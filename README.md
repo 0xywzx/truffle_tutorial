9f121cf0e08d35ee2c67294a5ee1c0452e7c5d11
shogo <35226144+shogoorg@users.noreply.github.com>
2019-02-15 21:30:07 +0900

    enhancement #9 Add implementation of ERC20

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # skills-ledger
 ブロックチェーンを使った資格証明ネットワーク
+
+## Quickstart
+- Install MetaMask
+- Install IPFS
+- Install IPFS Companion
+- Install OpenPGP (Optional)
+
+### Development Environment Setup
+
+You can set up your dev environment with:
+
+```sh
+npm install truffle@4.1.15
+git clone git@github.com:ethereum/skills-ledger.git
+cd skills-ledger
+npm install
+```
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,9 @@
-/**
- * @dev See https://github.com/truffle-box/react-box/blob/master/migrations/2_deploy_contracts.js
- */
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var Token = artifacts.require("./Token.sol");
 var NFTToken = artifacts.require("./NFTToken.sol");
 
 module.exports = function(deployer) {
   const name = "Property";
   const symbol = "PROPERTY";
-
-  deployer.deploy(SimpleStorage);
+  deployer.deploy(Token);
   deployer.deploy(NFTToken, name, symbol);
 };
